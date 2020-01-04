638cb9c0994ef1671a9020f7e22619c10b2df29c
csodaszarvas-gh <csodaszarvas.gh@gmail.com>
2020-01-04 23:41:38 +0100

    resistant-color: solution

--- resistor-color/src/main/java/ResistorColor.java
+++ resistor-color/src/main/java/ResistorColor.java
@@ -1,9 +1,24 @@
+import java.util.Arrays;
+
 class ResistorColor {
     int colorCode(String color) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return Arrays.asList(colors()).indexOf(color);
     }
 
     String[] colors() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String[] colors = {
+            "black",
+            "brown",
+            "red",
+            "orange",
+            "yellow",
+            "green",
+            "blue",
+            "violet",
+            "grey",
+            "white"
+        };
+
+        return colors;
     }
 }
