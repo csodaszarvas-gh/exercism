b233f23e58563060bcafcc6a84bebf9fea764ac0
csodaszarvas-gh <csodaszarvas.gh@gmail.com>
2020-01-04 22:18:51 +0100

    two-fer: implement solution

--- two-fer/src/main/java/Twofer.java
+++ two-fer/src/main/java/Twofer.java
@@ -1,5 +1,7 @@
 class Twofer {
     String twofer(String name) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if(name == null)
+            name = "you";
+        return "One for " + name + ", one for me.";
     }
 }
