a40839e897dbf2634bc961516d93c42afe9177d6
csodaszarvas-gh <csodaszarvas.gh@gmail.com>
2020-01-06 05:49:31 +0100

    armstrong-numbers: solution implemented

--- armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,9 +1,33 @@
+import java.util.ArrayList;
+
 class ArmstrongNumbers {
 
 	boolean isArmstrongNumber(int numberToCheck) {
 
-		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-		
+		if(numberToCheck == 0)
+			return true;
+
+		ArrayList<Integer> numbers = intToArrayList(numberToCheck);
+
+		int result = 0;
+		for(int i=0; i < numbers.size(); i++){
+			result += Math.pow(numbers.get(i), numbers.size());
+		}
+
+		if(result == numberToCheck)
+			return true;
+
+		return false;
+	}
+
+	ArrayList<Integer> intToArrayList(int number){
+		ArrayList<Integer> numbers = new ArrayList();
+		do {
+			numbers.add(number % 10);
+			number/=10;
+		} while (number > 0);
+
+		return numbers;
 	}
 
 }
