a3c575962c0a00196ef00a138af866693a982d7b
csodaszarvas-gh <csodaszarvas.gh@gmail.com>
2020-01-15 00:26:04 +0100

    pangram: solution

--- pangram/src/main/java/PangramChecker.java
+++ pangram/src/main/java/PangramChecker.java
@@ -1,7 +1,15 @@
 public class PangramChecker {
 
+    char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        input = input.toLowerCase();
+
+        for (char letter: alphabet) {
+            if (input.indexOf(letter) == -1)
+                return false;
+        }
+        return true;
     }
 
 }
