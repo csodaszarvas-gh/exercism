9601026e6b8298c93141db20af250474878e4cf7
csodaszarvas-gh <csodaszarvas.gh@gmail.com>
2020-01-15 00:15:17 +0100

    pangram: gitignore and init

--- grains/src/main/java/Grains.java
+++ grains/src/main/java/Grains.java
@@ -4,17 +4,15 @@ class Grains extends Exception {
     private static final String wrongSquareMessage = "square must be between 1 and 64";
 
     BigInteger grainsOnSquare(final int square) {
-        if (square > 0 && square < 65){
-            BigInteger base = new BigInteger("2");
-            return base.pow(square-1);
-        }
-        else
+        if (square < 1 || square > 64)
             throw new IllegalArgumentException(wrongSquareMessage);
+
+        return BigInteger.TWO.pow(square-1);
     }
 
     BigInteger grainsOnBoard() {
-        BigInteger result = new BigInteger("0");
-        for (int i=1; i<=64; i++)
+        BigInteger result = BigInteger.ONE;
+        for (int i=2; i<=64; i++)
             result = result.add(grainsOnSquare(i));
 
         return result;
