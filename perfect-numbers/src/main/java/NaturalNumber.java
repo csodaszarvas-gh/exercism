cbf0c44cc2da4b29f516086ccc2f469dc98ff725
csodaszarvas-gh <csodaszarvas.gh@gmail.com>
2020-02-21 06:07:46 +0100

    perfect-numbers: solution

--- perfect-numbers/src/main/java/NaturalNumber.java
+++ perfect-numbers/src/main/java/NaturalNumber.java
@@ -1,5 +1,34 @@
-class NaturalNumber {
+class NaturalNumber extends Exception {
 
+    static final String ERROR_MESSAGE = "You must supply a natural number (positive integer)";
 
+    int number;
+
+    NaturalNumber(int number) throws IllegalArgumentException {
+        if(number <= 0)
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        this.number = number;
+    }
+
+    Classification getClassification() {
+
+        int aliquotSum = 0;
+
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0)
+                aliquotSum += i;
+        }
+
+        Classification classification;
+
+        if (aliquotSum == number)
+            classification = Classification.PERFECT;
+        else if (aliquotSum > number)
+            classification = Classification.ABUNDANT;
+        else
+            classification = Classification.DEFICIENT;
+
+        return classification;
+    }
 
 }
